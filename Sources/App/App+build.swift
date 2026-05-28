6b0215cdbb92751c453e96e8305505f697c6baaf
Okunichiyou <173314355+Okunichiyou@users.noreply.github.com>
2026-05-29 08:42:52 +0900

    buildRouterを別ファイルに切り出す

--- Sources/App/App+build.swift
+++ Sources/App/App+build.swift
@@ -1,5 +1,4 @@
 import Configuration
-import Foundation
 import Hummingbird
 import Logging
 
@@ -21,35 +20,4 @@ func buildApplication(reader: ConfigReader) async throws -> some ApplicationProt
         logger: logger
     )
     return app
-}
-
-/// Build router
-func buildRouter() throws -> Router<AppRequestContext> {
-    let router = Router(context: AppRequestContext.self)
-    // Add middleware
-    router.addMiddleware {
-        // logging middleware
-        LogRequestsMiddleware(.info)
-        FileMiddleware("public")
-    }
-    // Add default endpoint
-    router.get("/") { _, _ in
-        var calender = Calendar.current
-        calender.timeZone = TimeZone(identifier: "Asia/Tokyo")!
-        let today = Date()
-
-        let baseAge = "15"
-
-        let currentYear = calender.component(.year, from: today)
-        let birthday = calender.date(from: DateComponents(year: currentYear, month: 6, day: 3))!
-        let age = currentYear - 2010 - (today < birthday ? 1 : 0)
-
-        var displayAge = baseAge
-        if age > 15 {
-            displayAge = baseAge + String(age)
-        }
-
-        return ProfilePage.render(age: displayAge)
-    }
-    return router
 }
---
+++ Sources/App/Router.swift
@@ -0,0 +1,30 @@
+import Foundation
+import Hummingbird
+
+func buildRouter() throws -> Router<AppRequestContext> {
+  let router = Router(context: AppRequestContext.self)
+  router.addMiddleware {
+    LogRequestsMiddleware(.info)
+    FileMiddleware("public")
+  }
+
+  router.get("/") { _, _ in
+    var calender = Calendar.current
+    calender.timeZone = TimeZone(identifier: "Asia/Tokyo")!
+    let today = Date()
+
+    let baseAge = "15"
+
+    let currentYear = calender.component(.year, from: today)
+    let birthday = calender.date(from: DateComponents(year: currentYear, month: 6, day: 3))!
+    let age = currentYear - 2010 - (today < birthday ? 1 : 0)
+
+    var displayAge = baseAge
+    if age > 15 {
+      displayAge = baseAge + String(age)
+    }
+
+    return ProfilePage.render(age: displayAge)
+  }
+  return router
+}
